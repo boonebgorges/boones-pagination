35492da16217ea9a6d1813c7483136e2f8daf756
boonebgorges <boonebgorges@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-23 18:35:37 +0000

    Reorganizes plugin so that metadata is stored in a loader file
    git-svn-id: http://plugins.svn.wordpress.org/boones-pagination/trunk@388178 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ loader.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+Plugin Name: Boone's Pagination
+Plugin URI: http://github.com/boonebgorges/boones-pagination
+Description: A handy, extensible pagination class for WordPress custom post type lists
+Version: 1.0
+Author: Boone B Gorges
+Author URI: http://boonebgorges.com
+Donate link: http://teleogistic.net/donate
+Licence: GPLv3
+*/
+
+/**
+ * The main plugin content is loaded this way so that the plugin header is isolated in its own file.
+ * This makes it easier to copy the class file wholesale to your own plugin or theme.
+ */
+require_once( dirname( __FILE__ ) . '/boones-pagination.php' );
+
+?>
